35164c08757a873c112b8df515bddf736ec82e4a
PSP22SCM36D <pdesai28@hawk.iit.edu>
2023-07-15 15:14:10 -0500

    Made 5th change to code

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('fourth change')
+        'body': json.dumps('5th change')
     }
